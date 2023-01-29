ca84247a9e4cfd689388272e30c6c2024fe5bd49
Borja Montseny <borja.montseny@gmail.com>
2023-01-29 22:15:32 +0100

    Initial commit

---
+++ src/a2Funcions/Ex01.java
@@ -0,0 +1,23 @@
+package a2Funcions;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Pàrquing: Una gran superfície comercial ofereix un pàrquing als seus clients.
+		 * La informació sobre l'ocupació del pàrquing es manté mitjançant una matriu
+		 * NxN. Cada component de la qual és cert si la plaça de pàrquing és ocupada
+		 * i fals en cas contrari.
+		 */
+
+		/*
+		 * 1. Crea una funció anomenada **percentatgePlacesLliuresFila** que donada la
+		 * matriu i el número de fila de la matriu, retorni el percentatge de places
+		 * lliures d'aquella fila.
+		 */
+
+	}
+
+}
